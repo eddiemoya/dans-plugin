082d54a7339d148f094b3a51cb01892ea2b2e6a6
Dan Crimmins <dbcrimmins@gmail.com>
2012-07-30 16:30:13 -0500

    Test

--- functions.php
+++ functions.php
@@ -1,5 +1,5 @@
 <?php
-
+//Functions
 function sso_logout_link($text) {
 	
 	echo '<a href="?ssologout&origin=' . urlencode('http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']) . '">' . $text . '</a>';
