90bdd827feb26057a1a24b6e09ffcad48da468d4
Dan Crimmins <dbcrimmins@gmail.com>
2012-08-20 18:48:11 -0500

    Tweaked logout url function to strip qs; fixed ol issue on profile page templates

--- functions.php
+++ functions.php
@@ -1,6 +1,39 @@
 <?php
 
+
+function strip_qs($url) {
+	
+	$url_parts = parse_url($url);
+	
+	if(count($url_parts)) {
+	
+		$url_out = $url_parts['scheme'] . '://' . $url_parts['host'];
+		
+			//Is there a port number?
+			if(isset($url_parts['port'])) {
+				
+				$url_out .= ':' . $url_parts['port'];
+			}
+			
+			//Is there a uri?
+			if(isset($url_parts['path'])) {
+				
+				$url_out .= $url_parts['path'];
+			} 
+			
+			
+				return $url_out;
+				
+		} else { //$url was not a URL, return it
+			
+			return $url;
+		}
+}
+
 function sso_logout_link($text) {
 	
-	echo '<a href="?ssologout&origin=' . urlencode('http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']) . '" title="Logout" class="bold">' . $text . '</a>';
+	$current_url = (! empty($_SERVER['HTTPS']) ? 'https://' : 'http://') . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+	
+	
+	echo '<a href="?ssologout&origin=' . urlencode(strip_qs($current_url)) . '" title="Logout" class="bold">' . $text . '</a>';
 }
