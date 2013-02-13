117870317b3578bc66df48c821a15a6a2d4c9c16
Dan Crimmins <dbcrimmins@gmail.com>
2013-02-13 16:54:21 -0600

    changes to options class

--- class/sso_options.php
+++ class/sso_options.php
@@ -13,6 +13,7 @@ class SSO_Options {
 		
 		$this->_get_options();
 		$this->_set_service_url();
+		$this->_set_endpoint();
 	}
 	
 	public function factory() {
@@ -51,6 +52,62 @@ class SSO_Options {
 	protected function _set_service_url() {
 		
 		$this->service = (! empty($_SERVER['HTTPS']) ? 'https://' : 'http://') . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+	}
+	
+	protected function _set_endpoint() {
+		
+		$this->endpoint = $this->_endpoints[$this->environment];
+	}
+	
+	/**
+	 * Given a URL and a querystring returns URL with querystring appended to URL. 
+	 * Will include port number if present in url. If a querystring is present in $url, will
+	 * cleanly append $qs to end of current querystring.
+	 * 
+	 * Usage: url_append_qs('http://mydomain.com/', 'var1=something'), outputs: http://mydomain.com/?var1=something
+	 * 			 url_append_qs('http://mydomain.com/?var1=something', 'var2=somethingelse'), outputs: http://mydomain.com/?var1=something&var2=somethingelse
+	 * 
+	 * @param string $qs - Examples: '&var1=val1&var2=val2' --leading ampersand is optional
+	 * @param string $url
+	 * @return string - $url with appended  $qs
+	 * @access private
+	 */
+	public function url_append_qs($qs, $url) {
+	
+	$url_parts = parse_url($url);
+	
+	if(count($url_parts)) {
+	
+		$url_out = $url_parts['scheme'] . '://' . $url_parts['host'];
+		
+			//Is there a port number?
+			if(isset($url_parts['port'])) {
+				
+				$url_out .= ':' . $url_parts['port'];
+			}
+			
+			//Is there a uri?
+			if(isset($url_parts['path'])) {
+				
+				$url_out .= $url_parts['path'];
+			} 
+			
+			//Is there a querystring
+			if(isset($url_parts['query'])) {
+				
+				$url_out .= '?' . $url_parts['query'] . '&' . ltrim($qs, '&');
+				
+			} else {
+				
+				$url_out .= '?' . ltrim($qs, '&');
+			}
+			
+				return $url_out;
+				
+		} else { //$url was not a URL, return it
+			
+			return $url;
+		}
 	}
 	
 	
