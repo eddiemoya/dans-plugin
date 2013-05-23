466b7273183fccce1dd308da6c2d9e3330f8262a
Dan Crimmins <dbcrimmins@gmail.com>
2013-05-23 15:49:44 -0500

    fixed wp-load.php require on login.php and service.php to work on all envs, even one using sym links

--- public/login.php
+++ public/login.php
@@ -1,10 +1,6 @@
 <?php //Handles login and registration
 
-if(stripos(__FILE__, 'wordpress/') !== false) {
-        $file =  substr(__FILE__, 0, (stripos(__FILE__, 'wordpress/') + 10)) . 'wp-load.php';
-} else {
-        $file = substr(__FILE__, 0, (stripos(__FILE__, 'wp-content'))) . 'wp-load.php';
-}
+$file = substr(__FILE__, 0, (stripos(__FILE__, 'wp-content/'))) . 'wp-load.php';
 
 require_once $file;
 
