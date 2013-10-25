ae3c4571fa209b60714686fa0080a3b81e34a3d1
Dan Crimmins <dbcrimmins@gmail.com>
2013-10-25 15:18:04 -0500

    trying window.location.replace()

--- views/redirect.php
+++ views/redirect.php
@@ -1,4 +1,5 @@
 <?php //JS to redirect, send $url?>
 <script type="text/javascript">
-window.location.href = '<?php echo $url;?>';
+//window.location.href = '<?php //echo $url;?>';
+window.location.replace('<?php echo $url;?>');
 </script>
