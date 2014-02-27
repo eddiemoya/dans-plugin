189a3416fdb35a6d9694363f2efd2931ff8f3c37
Eddie Moya <eddie.moya@gmail.com>
2014-02-27 16:18:31 -0600

    changing the responsys request class to set launch_date and side_code date dynamically, based on the date of the request

--- class/controller/sso_responsys_request.php
+++ class/controller/sso_responsys_request.php
@@ -7,6 +7,10 @@ class SSO_Responsys_Request extends SSO_Base_Request {
 	
 	protected $_username;
 
+	protected $_side_code_date;
+
+	protected $_launch_date
+
 		
 	public function __construct($email, $user_id) {
 		
@@ -15,6 +19,7 @@ class SSO_Responsys_Request extends SSO_Base_Request {
 		$this->_username();
 		$this->_endpoint();
 		$this->_url(false);
+		$this->set_dates();
 	}
 	
 	public static function factory($email, $user_id) {
@@ -28,17 +33,21 @@ class SSO_Responsys_Request extends SSO_Base_Request {
 		$this->_username = $email_parts[0];
 	}
 	
+	protected function set_dates(){
+		$this->_side_code_date = date('Y-m-d');
+		$this->_launch_date = date('Ymd');
+	}
 	protected function _endpoint() {
 		
 		$site = (stripos(get_bloginfo('name'), 'sears') !== false) ? 'MS' : 'MK';
 		
 		if($site == 'MS') { //Sears
 			
-			$this->_endpoint = "https://value.sears.com/pub/rf?_ri_=X0Gzc2X%3DWQpglLjHJlTQGo2trMyHvzcdfAizdCOgaza4CJ1PldMVwjpnpgHlpgneHmgJoXX0Gzc2X%3DWQpglLjHJlTQGhE2WmfvtPtzfCzgd2MuuWFLcuamIG&EMAIL_ADDRESS_={$this->_email}&SCREEN_NAME={$this->_username}&SID_CODE=ITx20120921TriggeredSRSMCWelcome&LAUNCH_DATE=2012-09-21&OPT_TYPE_CODE=MS&USERID={$this->_user_id}";
+			$this->_endpoint = "https://value.sears.com/pub/rf?_ri_=X0Gzc2X%3DWQpglLjHJlTQGo2trMyHvzcdfAizdCOgaza4CJ1PldMVwjpnpgHlpgneHmgJoXX0Gzc2X%3DWQpglLjHJlTQGhE2WmfvtPtzfCzgd2MuuWFLcuamIG&EMAIL_ADDRESS_={$this->_email}&SCREEN_NAME={$this->_username}&SID_CODE=ITx{$this->_sid_code_date}TTriggeredSRSMCWelcome&LAUNCH_DATE={$this->_launch_date}&OPT_TYPE_CODE=MS&USERID={$this->_user_id}";
 			
 		} else { //Kmart
 			
-			$this->_endpoint = "https://kmart.rsys2.net/pub/rf?_ri_= X0Gzc2X%3DWQpglLjHJlTQGrO29WN3s0tgTzb4ADPryLRJzduOVwjpnpgHlpgneHmgJoXX0Gzc2X%3DWQpglLjHJlTQGNJaaXyBOGyYoJeUczdzbyNYBkMn&EMAIL_ADDRESS={$this->_email}&SCREEN_NAME={$this->_username}&SID_CODE=ITx20120921TriggeredMKwelcome&LAUNCH_DATE=2012-09-21&OPT_TYPE_CODE=MK&USERID={$this->_user_id}";
+			$this->_endpoint = "https://kmart.rsys2.net/pub/rf?_ri_= X0Gzc2X%3DWQpglLjHJlTQGrO29WN3s0tgTzb4ADPryLRJzduOVwjpnpgHlpgneHmgJoXX0Gzc2X%3DWQpglLjHJlTQGNJaaXyBOGyYoJeUczdzbyNYBkMn&EMAIL_ADDRESS={$this->_email}&SCREEN_NAME={$this->_username}&SID_CODE=ITx{$this->_sid_code_date}TriggeredMKwelcome&LAUNCH_DATE={$this->_launch_date}&OPT_TYPE_CODE=MK&USERID={$this->_user_id}";
 		}
 	}
 	
